9049246f0520972b5e70d57a58c2a1e30a2bbe7f
Abdullah Al Tabbal <xlp1abode@gmail.com>
2023-10-02 01:09:55 +0200

    set up some files

---
+++ app/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import Image from "next/image"
+import Link from "next/link"
+import Bean2 from "./Bean2.png"
+
+export default function Navbar() {
+  return (
+    <nav>
+      <Image
+        alt="bean-image"
+        src={Bean2}
+        width={70}
+        quality={100}
+        placeholder="blur"
+      />
+      <Link href="/"> Dashboard </Link>
+      <Link href="/tickets"> Tickets </Link>
+    </nav>
+  )
+}
